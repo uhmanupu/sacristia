31b9a66ba2315fa9129c05bac0402fc9742ffa41
uhmanupu <emanuelpu@gmail.com>
2025-12-16 22:05:47 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,10 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    // Carrega as variáveis de ambiente (localmente do .env ou do GitHub Actions)
+    // Carrega as variáveis de ambiente com base no modo (development ou production)
     const env = loadEnv(mode, process.cwd(), '');
     
     return {
-      // Define o caminho base para o GitHub Pages
       base: '/sacristia/', 
       server: {
         port: 3000,
@@ -15,7 +14,7 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       define: {
-        // Expõe a chave para o código frontend
+        // Expõe a chave para o seu serviço GeminiService.ts
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
